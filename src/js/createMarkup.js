1836c2a525e3cc901f80a3cf1360ccdccbe8630d
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-10-15 09:54:49 +0300

    Updates markup

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -1,54 +1,28 @@
 export function createMarkup(photos) {
   if (!photos) return;
 
-  return photos.reduce(
-    (
-      acc,
-      { webformatURL, largeImageURL, tags, likes, views, comments, downloads }
-    ) => {
-      return (
-        acc +
-        /*html*/ `
-      <a href="${largeImageURL}" class="col-sm-6 col-lg-4 col-xl-3 photo-card">
-        <img
-          src="${webformatURL}"
-          alt="${tags}"
-          loading="lazy"
-          height="300"
-        />
+  return photos.reduce((acc, photo) => {
+    const {
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    } = photo;
+
+    return (
+      acc +
+      `<a href="${largeImageURL}" class="col-sm-6 col-lg-4 col-xl-3 photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" height="300" />
         <div class="info">
-          <p class="info-item" title="Likes">
-            <!-- <b>Likes</b> -->
-            <svg width="14" height="14">
-              <use href="./images/icons.svg#heart" />
-            </svg>
-            ${likes}
-          </p>
-          <p class="info-item" title="Views">
-            <!-- <b>Views</b> -->
-            <svg width="14" height="14">
-              <use href="./images/icons.svg#eye" />
-            </svg>
-            ${views}
-          </p>
-          <p class="info-item" title="Comments">
-            <!-- <b>Comments</b> -->
-            <svg width="14" height="14">
-              <use href="./images/icons.svg#bubbles" />
-            </svg>
-            ${comments}
-          </p>
-          <p class="info-item" title="Downloads">
-            <!-- <b>Downloads</b> -->
-            <svg width="14" height="14">
-              <use href="./images/icons.svg#box-add" />
-            </svg>
-            ${downloads}
-          </p>
+          <p class="info-item" title="Likes"><b>Likes</b>${likes}</p>
+          <p class="info-item" title="Views"><b>Views</b>${views}</p>
+          <p class="info-item" title="Comments"><b>Comments</b>${comments}</p>
+          <p class="info-item" title="Downloads"><b>Downloads</b>${downloads}</p>
         </div>
       </a>`
-      );
-    },
-    ''
-  );
+    );
+  }, '');
 }
